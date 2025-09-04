95e3abe8120fdd054368e86b272c7278ceb1f54b
Harshsaxena13 <harshsaxena1189@gmail.com>
2025-09-04 09:22:30 +0000

    variables

---
+++ basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=12345
+let accountEmail="harsh123@gmail.com"
+var accountpassword=9548
+let accdetail
+
+console.log(accountId);
+
+
+accountEmail="saxena123@gmail.com"
+accountpassword="7520"
+accountcity="rampur"
+
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accdetail]);
+
+
+
+
