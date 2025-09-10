28dd7a4261079380de663cde69b2a7b87e5f9c0d
Harsh Saxena <harshsaxena1189@gmail.com>
2025-09-10 10:01:15 +0000

    maths and num

---
+++ basics/06_num.js
@@ -0,0 +1,24 @@
+console.log(Math);
+
+console.log(Math.random());
+console.log((Math.random()*10)+10);
+console.log(Math.floor((Math.random()*10)+10));
+
+const min= "10"
+const max= "22"
+console.log((Math.floor((Math.random()*(max-min+1))+min)));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+
+
